74660ff6aeff44db6ee49dc8081f1225c09fbe83
Jonathan <jndayele@gmail.com>
2024-10-28 03:55:50 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import QuoteGenerator from "./Component/quoteGenerator";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <QuoteGenerator />
+    </div>
+  );
+}
+
+export default App;
